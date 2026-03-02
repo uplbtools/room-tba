4e9b2cd77e89eef8cb1fe84139b469751dc8c050
Kenramiscal1106 <code.ramiscalken@gmail.com>
2026-03-02 19:20:17 +0800

    add pwa support

--- astro-room/astro.config.mjs
+++ astro-room/astro.config.mjs
@@ -2,11 +2,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-import { VitePWA } from "vite-plugin-pwa";
+import AstroPWA from "@vite-pwa/astro";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), svelte()],
-  vite: {
-    plugins: [VitePWA({ registerType: "autoUpdate" })]
-  }
+  integrations: [react(), svelte(), AstroPWA({
+    registerType: "prompt",
+  })],
 });
