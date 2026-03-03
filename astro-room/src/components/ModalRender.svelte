6c292c6153f408e028309fc2419cecbfff55b6ea
Kenramiscal1106 <code.ramiscalken@gmail.com>
2026-03-03 17:50:57 +0800

    remove folder migration

--- astro-room/src/components/ModalRender.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import RoomModal from "./Modal.svelte";
-</script>
-
-<RoomModal />
--- astro-room/src/lib/db.ts
+++
@@ -1,5 +0,0 @@
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
-
-const client = new Database(process.cwd() + "/data/info.db")
-export const db = drizzle({client});
