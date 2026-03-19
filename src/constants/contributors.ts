e1b911b0a1db5d0e9e9c0a8ced77ddcd059909d8
Kenramiscal1106 <code.ramiscalken@gmail.com>
2026-03-19 16:36:41 +0800

    update contributors

---
+++ src/constants/contributors.ts
@@ -0,0 +1,12 @@
+import type { ContributorInfo } from "../lib/types";
+
+export const contributors: ContributorInfo[] = [
+  {name: "Simonee Ezekiel Mariquit", href: "https://stimmie.dev"}, 
+  {name: "Ken Daniele Ramiscal", href: "https://kendan.dev"}, 
+  {name: "Kalinaw Lukas Aom Bebis", href: "https://lukasbebis.com"},
+  {name: "Eunice Almeyda"},
+  {name: "Dandel Oliver Prado"},
+  {name: "Niño Anthony Marmeto"},
+  {name: "Rosh Almario"},
+  {name: "Mary Gwyneth Telmosa"}
+]
