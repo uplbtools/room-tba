d41745df9d0d54cf63f174f592ae4575adb707a5
Kenramiscal1106 <code.ramiscalken@gmail.com>
2026-03-27 23:40:43 +0800

    add emphasis to search input and add event to suggestion

--- src/components/svelte/Suggestion.svelte
+++ src/components/svelte/Suggestion.svelte
@@ -1,18 +1,35 @@
 <script lang="ts">
-  import type { QueryStoreState } from "../../lib/store.svelte";
+  import { queryStore, type QueryStoreState } from "../../lib/store.svelte";
 
   const suggestion: {
     value: string;
     category: Exclude<QueryStoreState["category"], null>;
   } = $props();
+  const pattern = $derived(new RegExp(`(${queryStore.value.trim()})`, "gi"));
+  function highlightSearch(original: string, pattern: RegExp): string {
+    return queryStore.value.length < 2
+      ? original
+      : original.replaceAll(pattern, (substr) => `<strong>${substr}</strong>`);
+  }
+
+  function handleSuggestionClick(
+    event: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement },
+  ) {
+    queryStore.updateQuery({
+      type: "result",
+      category: suggestion.category,
+    });
+    queryStore.value = suggestion.value;
+  }
 </script>
 
-<div class="suggestion">
-  <div>{suggestion.value}</div>
-</div>
+<button class="suggestion" onclick={handleSuggestionClick}>
+  <div>{@html highlightSearch(suggestion.value, pattern)}</div>
+</button>
 
 <style>
   .suggestion {
+    all: unset;
     padding: 0.5rem 1rem;
     &:hover {
       background-color: hsl(0, 0%, 90%);
