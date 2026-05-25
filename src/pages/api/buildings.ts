777ece129e5cf740d9c356eb0d29147b3ec6871d
Kenramiscal1106 <code.ramiscalken@gmail.com>
2026-05-26 07:26:17 +0800

    feat: add API routes for data in DB

---
+++ src/pages/api/buildings.ts
@@ -0,0 +1,7 @@
+import { APIRoute } from "astro";
+import { getAllBuildings } from "../../lib/services/map-data-service";
+
+export const GET = (async (_) => {
+  const data = await getAllBuildings();
+  return new Response(JSON.stringify(data));
+}) satisfies APIRoute;
