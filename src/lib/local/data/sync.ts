eceef623eb883e26a0c67b1b21a961befcce24cd
Kenramiscal1106 <code.ramiscalken@gmail.com>
2026-05-27 16:39:12 +0800

    feat: add utils for syncing data

---
+++ src/lib/local/data/sync.ts
@@ -0,0 +1,75 @@
+import {
+  BuildingData,
+  CollegeData,
+  DivisionData,
+  DormData,
+  RoomData,
+  RoomPosition,
+} from "../../types";
+
+export const API_ROUTES = [
+  "/buildings",
+  "/colleges",
+  "/divisions",
+  "/dorms",
+  "/rooms",
+] as const;
+
+type ReturnAllData = [
+  BuildingData[],
+  CollegeData[],
+  DivisionData[],
+  DormData[],
+  RoomData[],
+];
+
+export async function syncData(): Promise<{
+  success: boolean;
+  error: Error | null;
+}> {
+  const localSyncHash = localStorage.getItem("sync-hash");
+  const current = await (await fetch("/api/check-sync")).text();
+
+  if (localSyncHash === null || localSyncHash !== current) {
+    try {
+      const [buildings, colleges, divisions, dorms, rooms] =
+        (await getAllData()) as ReturnAllData;
+
+      const dataLength = gettotalDataLength(
+        buildings,
+        colleges,
+        divisions,
+        dorms,
+        rooms,
+      );
+
+      return {
+        success: true,
+        error: null,
+      };
+    } catch {
+      const err = new Error("Failed to sync data");
+      return {
+        success: false,
+        error: err,
+      };
+    }
+  }
+  return {
+    success: true,
+    error: null,
+  };
+}
+
+export async function getAllData() {
+  return Promise.all(API_ROUTES.map((url) => getJSONFetch(url)));
+}
+
+export async function getJSONFetch(url: string) {
+  const req = await fetch(url);
+  return await req.json();
+}
+
+export function gettotalDataLength(...arrays: any[][]): number {
+  return arrays.reduce((acc, curr) => acc + curr.length, 0);
+}
