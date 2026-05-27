d5e8228309830d3936fc1cc57e2f8df9285fe119
Kenramiscal1106 <code.ramiscalken@gmail.com>
2026-05-27 17:11:26 +0800

    refactor: change data flow from server to client

--- src/components/svelte/AppProvider.svelte
+++ src/components/svelte/AppProvider.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
-  import { AppContextData, setAppData } from "../../lib/context";
-  import Entry from "./Entry.svelte";
+  import { onMount } from "svelte";
+  import { isBrowser } from "es-toolkit";
+  import { syncData } from "../../lib/local/data/sync";
+  // import { AppContextData, setAppData } from "../../lib/context";
+  // import Entry from "./Entry.svelte";
+  import { getLocalBuildings, getLocalColleges } from "../../lib/local/data/utils";
 
-  type Props = AppContextData;
-  const appData: Props = $props();
+  // type Props = AppContextData;
 
-  // svelte-ignore state_referenced_locally
-  setAppData(appData);
+  onMount(async () => {
+    if (isBrowser()) {
+      syncData();
+    }
+  });
 </script>
 
-<Entry />
+<!-- <Entry /> -->
