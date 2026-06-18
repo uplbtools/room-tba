167ed83e322560938690a593330e47076bec8d18
Kenramiscal1106 <code.ramiscalken@gmail.com>
2026-06-18 14:24:25 +0800

    feat: update api route for rooms - added search parameter checking/parsing - created functions for fetching depending on what its based on

--- src/lib/services/map-data-service.ts
+++ src/lib/services/map-data-service.ts
@@ -57,6 +57,102 @@ export async function getAllRooms(): Promise<RoomData[]> {
   }
 }
 
+export async function getBuildingRooms(
+  buildingId: number,
+): Promise<RoomData[]> {
+  try {
+    const data = await db
+      .select({
+        id: roomsTable.id,
+        code: roomsTable.roomCode,
+        directions: roomsTable.directions,
+        building: {
+          name: buildingsTable.buildingName,
+          lat: buildingsTable.lat,
+          lon: buildingsTable.lon,
+          directions: buildingsTable.directions,
+        },
+        collegeName: collegesTable.collegeName,
+        divisionName: divisionsTable.divisionName,
+        buildingId: roomsTable.buildingId,
+        collegeId: roomsTable.collegeId,
+        divisionId: roomsTable.divisionId,
+      })
+      .from(roomsTable)
+      .leftJoin(buildingsTable, eq(buildingsTable.id, roomsTable.buildingId))
+      .leftJoin(collegesTable, eq(collegesTable.id, roomsTable.collegeId))
+      .leftJoin(divisionsTable, eq(divisionsTable.id, roomsTable.divisionId))
+      .where(eq(roomsTable.buildingId, buildingId));
+    return data;
+  } catch (e) {
+    console.error("Error: ", e);
+    throw new Error("Failed to fetch rooms");
+  }
+}
+export async function getCollegeRooms(collegeId: number): Promise<RoomData[]> {
+  try {
+    const data = await db
+      .select({
+        id: roomsTable.id,
+        code: roomsTable.roomCode,
+        directions: roomsTable.directions,
+        building: {
+          name: buildingsTable.buildingName,
+          lat: buildingsTable.lat,
+          lon: buildingsTable.lon,
+          directions: buildingsTable.directions,
+        },
+        collegeName: collegesTable.collegeName,
+        divisionName: divisionsTable.divisionName,
+        buildingId: roomsTable.buildingId,
+        collegeId: roomsTable.collegeId,
+        divisionId: roomsTable.divisionId,
+      })
+      .from(roomsTable)
+      .leftJoin(buildingsTable, eq(buildingsTable.id, roomsTable.buildingId))
+      .leftJoin(collegesTable, eq(collegesTable.id, roomsTable.collegeId))
+      .leftJoin(divisionsTable, eq(divisionsTable.id, roomsTable.divisionId))
+      .where(eq(roomsTable.collegeId, collegeId));
+    return data;
+  } catch (e) {
+    console.error("Error: ", e);
+    throw new Error("Failed to fetch rooms");
+  }
+}
+export async function getDivisionRooms(
+  divisionId: number,
+): Promise<RoomData[]> {
+  try {
+    const data = await db
+      .select({
+        id: roomsTable.id,
+        code: roomsTable.roomCode,
+        directions: roomsTable.directions,
+        building: {
+          name: buildingsTable.buildingName,
+          lat: buildingsTable.lat,
+          lon: buildingsTable.lon,
+          directions: buildingsTable.directions,
+        },
+        collegeName: collegesTable.collegeName,
+        divisionName: divisionsTable.divisionName,
+        buildingId: roomsTable.buildingId,
+        collegeId: roomsTable.collegeId,
+        divisionId: roomsTable.divisionId,
+      })
+      .from(roomsTable)
+      .leftJoin(buildingsTable, eq(buildingsTable.id, roomsTable.buildingId))
+      .leftJoin(collegesTable, eq(collegesTable.id, roomsTable.collegeId))
+      .leftJoin(divisionsTable, eq(divisionsTable.id, roomsTable.divisionId))
+      .where(eq(roomsTable.divisionId, divisionId));
+
+    return data;
+  } catch (e) {
+    console.error("Error: ", e);
+    throw new Error("Failed to fetch rooms");
+  }
+}
+
 export async function getAllColleges(): Promise<CollegeData[]> {
   try {
     const data = await db.select().from(collegesTable);
@@ -107,6 +203,6 @@ export async function getAllDorms(): Promise<DormData[]> {
     return data;
   } catch (e) {
     console.error("Error: ", e);
-    throw new Error("Failed to fetch data for dorms")
+    throw new Error("Failed to fetch data for dorms");
   }
 }
--- src/pages/api/rooms.ts
+++ src/pages/api/rooms.ts
@@ -1,10 +1,62 @@
 import { APIRoute } from "astro";
-import { getAllRooms } from "../../lib/services/map-data-service";
+import {
+  getBuildingRooms,
+  getCollegeRooms,
+  getDivisionRooms,
+} from "../../lib/services/map-data-service";
+import { RoomData } from "../../lib/types";
+// import { getAllRooms } from "../../lib/services/map-data-service";
 
-export const GET = (async () => {
-  const data = await getAllRooms();
+export const GET = (async ({ url }) => {
+  const searchKeys = Array.from(url.searchParams.keys());
+  if (searchKeys.length !== 1)
+    return new Response("Error with your request", {
+      status: 400,
+      statusText: "Bad request",
+    });
 
-  return new Response(JSON.stringify(data), {
-    headers: [["Access-Control-Allow-Origin", "http://localhost:4321"]],
-  });
+  const searchField = searchKeys[0] as string;
+  const fieldSets = ["building_id", "college_id", "division_id"];
+  if (!fieldSets.includes(searchField))
+    return new Response("404 not found", {
+      status: 404,
+      statusText: "Not Found",
+    });
+
+  const id = parseInt(url.searchParams.get(searchField) as string);
+  let data: null | RoomData[] = null;
+  console.log(url.searchParams.get(searchField), id);
+  if (isNaN(id))
+    return new Response("Error with your request", {
+      status: 400,
+      statusText: "Bad request",
+    });
+
+  switch (searchField) {
+    case "building_id":
+      data = await getBuildingRooms(id);
+      break;
+    case "college_id":
+      data = await getCollegeRooms(id);
+      break;
+    case "division_id":
+      data = await getDivisionRooms(id);
+      break;
+  }
+
+  if (data?.length === 0) return new Response("No data exists", {
+        status: 404,
+        statusText: "query not found"
+    });
+  return new Response(
+    JSON.stringify({
+      data,
+      success: true,
+    }, null, 2),
+    {
+      headers: [["Access-Control-Allow-Origin", "http://localhost:4321"]],
+    },
+  );
 }) satisfies APIRoute;
+
+export const prerender = false;
