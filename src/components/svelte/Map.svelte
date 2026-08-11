1f8192d5633d206589c5718de05c2eee49e7987a
smmariquit <smmariquit@up.edu.ph>
2026-08-11 12:47:33 +0800

    fix(map): support MapLibre 6 module exports

--- src/components/svelte/Map.svelte
+++ src/components/svelte/Map.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { MapLibre, Marker } from "svelte-maplibre";
-  import maplibregl from "maplibre-gl";
+  import * as maplibregl from "maplibre-gl";
   import maplibreWorkerUrl from "maplibre-gl/dist/maplibre-gl-csp-worker.js?url";
 
   // In production Vite inlines maplibre into the app chunk and boots its
